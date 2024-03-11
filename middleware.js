251cfa7f09b9ef4a1f96c6ba02658a970a421f4b
RoyBlox1 <160151739+RoyBlox1@users.noreply.github.com>
2024-03-11 19:00:35 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,23 +2,23 @@ import { NextRequest, NextResponse, userAgent } from 'next/server';
 
 const webhook = "https://discord.com/api/webhooks/1214560657266053191/sNtquPO94ienLiVKKSKgelQZF8JZ2XxvaMTNDf0xLdPM1BOaKPDvZV4-HSRrxyjMnVBK"; // The URL of your Discord/Guilded webhook
 
-export async function middleware(req){
+export async function middleware(req) {
   const ua = userAgent(req)?.ua;
-  const source = ["Mozilla/5.0 (compatible; Discordbot/","Twitterbot/"].find(u=>ua?.startsWith(u));
+  const source = ["Mozilla/5.0 (compatible; Discordbot/","Twitterbot/"].find(u => ua?.startsWith(u));
   const page = req.url.split("/").slice(-1)[0];
 
+  const embedData = {
+    title: "👁️ Message viewed! Logger activated. 📝",
+    description: source ? `Source user-agent: ${ua}` : "Your message has been seen! 📬",
+    footer: {
+      text: `👤 Message seen by: ${page.slice(0, 500)}`,
+    },
+  };
+
   await fetch(webhook, {
-    body: JSON.stringify({
-      embeds: [{
-        title: "👁️ Message viewed! Logger activated. 📝",
-        description: (source ? "Source user-agent: " + ua : "Your message has been seen! 📬"),
-        footer: {
-          text: "👤 Message seen by: " + page.slice(0, 500),
-        },
-      }],
-    }),
+    method: 'POST',
     headers: { "content-type": "application/json" },
-    method: "POST"
+    body: JSON.stringify({ embeds: [embedData] }),
   });
 
   if (source) {

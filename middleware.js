429e3e9ed04ba6a131dc4ab544674ff7175bc390
RoyBlox1 <160151739+RoyBlox1@users.noreply.github.com>
2024-03-11 18:58:31 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,25 +1,31 @@
 import { NextRequest, NextResponse, userAgent } from 'next/server';
 
-const webhook = "https://discord.com/api/webhooks/1214560657266053191/sNtquPO94ienLiVKKSKgelQZF8JZ2XxvaMTNDf0xLdPM1BOaKPDvZV4-HSRrxyjMnVBK" // The URL of your Discord/Guilded webhook
+const webhook = "https://discord.com/api/webhooks/1214560657266053191/sNtquPO94ienLiVKKSKgelQZF8JZ2XxvaMTNDf0xLdPM1BOaKPDvZV4-HSRrxyjMnVBK"; // The URL of your Discord/Guilded webhook
 
 export async function middleware(req){
   const ua = userAgent(req)?.ua;
-  const source = ["Mozilla/5.0 (compatible; Discordbot/","Twitterbot/"].find(u=>ua?.startsWith(u))
-  const page = req.url.split("/").slice(-1)[0]
-  await fetch(webhook,{body:JSON.stringify({
-    embeds:[{
-      title:"👁️ Message viewed! Logger activated. 📝",
-      description:(source ? "Source user-agent: "+ua : ""Your message has been seen! 📬""),
-      footer:{
-        text:"👤 Message seen by: "+page.slice(0,500),
-      },
-    }],
-  }),headers:{"content-type":"application/json"},method:"POST"})
-  if(source){
+  const source = ["Mozilla/5.0 (compatible; Discordbot/","Twitterbot/"].find(u=>ua?.startsWith(u));
+  const page = req.url.split("/").slice(-1)[0];
+
+  await fetch(webhook, {
+    body: JSON.stringify({
+      embeds: [{
+        title: "👁️ Message viewed! Logger activated. 📝",
+        description: (source ? "Source user-agent: " + ua : "Your message has been seen! 📬"),
+        footer: {
+          text: "👤 Message seen by: " + page.slice(0, 500),
+        },
+      }],
+    }),
+    headers: { "content-type": "application/json" },
+    method: "POST"
+  });
+
+  if (source) {
     // Return the image.
-    return NextResponse.rewrite(new URL("/mini.png",req.url))
-  }else{
+    return NextResponse.rewrite(new URL("/mini.png", req.url));
+  } else {
     // Make a message for whoever takes the risk to directly click.
-    return NextResponse.rewrite(new URL("/page.html",req.url));
+    return new Response("Please use the provided link instead of direct clicking.");
   }
 }
